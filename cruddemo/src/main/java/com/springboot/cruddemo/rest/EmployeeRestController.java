a279bba93ea8f2942861e330b6c904dad1a9c38f
Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>
2021-01-01 19:54:22 +0530

    Rest Controller Method to Find Employee by Id

--- cruddemo/src/main/java/com/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +28,15 @@ public class EmployeeRestController {
 	public List<Employee> findAll() {
 		return employeeService.findAll();
 	}
+
+	// add mapping for GET /employees/{employeeId}
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId) {
+		Employee theEmployee = employeeService.findById(employeeId);
+		if (theEmployee == null) {
+			throw new RuntimeException("Employee Id not Found " + employeeId);
+		}
+		return theEmployee;
+	}
+
 }
