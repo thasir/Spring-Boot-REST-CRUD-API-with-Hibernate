23fdad53e8c9f24c11b02cb58e0f724a265ded5c
Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>
2021-01-01 19:31:54 +0530

    Refactored the Code to use Service Layer and implemented GET all method

--- cruddemo/src/main/java/com/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/springboot/cruddemo/rest/EmployeeRestController.java
@@ -9,21 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.cruddemo.dao.EmployeeDAO;
 import com.springboot.cruddemo.entity.Employee;
+import com.springboot.cruddemo.service.EmployeeService;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
-	private EmployeeDAO employeeDAO;
+	private EmployeeService employeeService;
 
 	// quick and dirty inject employee DAo
 	@Autowired
-	public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-		employeeDAO = theEmployeeDAO;
+	public EmployeeRestController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
 	}
 
 	// expose "/employees" and return list of employees
 	@GetMapping("/employees")
 	public List<Employee> findAll() {
-		return employeeDAO.findAll();
+		return employeeService.findAll();
 	}
 }
