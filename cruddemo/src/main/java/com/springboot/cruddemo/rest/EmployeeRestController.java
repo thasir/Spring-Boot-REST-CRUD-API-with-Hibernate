35b8e9de9340708d6b96ac54ca5ea758e8faaaa1
Shaik Thasir Ahmed <67335450+thasir@users.noreply.github.com>
2021-01-01 20:18:12 +0530

    Rest Controller Method to Update Employee

--- cruddemo/src/main/java/com/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/springboot/cruddemo/rest/EmployeeRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +47,13 @@ public class EmployeeRestController {
 		employeeService.save(theEmployee);
 		return theEmployee;
 
+	}
+
+	// add a PUT mapping update employee
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
+		employeeService.save(theEmployee);
+		return theEmployee;
 	}
 
 }
